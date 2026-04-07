01f60d14d083a4baef04ed7c63bb568eec3863d7
Mahesha V P <maheshavp12@gmail.com>
2026-04-07 20:15:40 +0530

    super() keyword

--- oops/super.py
+++ oops/super.py
@@ -1,1 +1,36 @@
 #super() method
+
+class Shape:
+    def __init__(self,color,is_filled):
+        self.color = color
+        self.is_filled = is_filled
+
+    def describe(self):
+        print(f"It is {self.color} and {"filled" if self.is_filled else "Not filled"}")
+
+class Circle(Shape):
+    def __init__(self, color, is_filled,radius):
+        super().__init__(color, is_filled)
+        self.radius = radius
+
+    def cal(self):
+        print(f"The area of circle is : {3.14 * self.radius * self.radius}")
+
+class Square(Shape):
+    def __init__(self, color, is_filled,width):
+        super().__init__(color, is_filled)
+        self.width = width
+
+    def cal(self):
+        print(f"Area of square is : {self.width * self.width}")
+
+circle = Circle("blue",True,5)
+square = Square("Green",False,4)
+
+circle.describe()
+circle.cal()
+ 
+print("\n")
+
+square.describe()
+square.cal()
