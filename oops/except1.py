9f43593b960312bdda453cf68d4f4f1ff1d029f3
Mahesha V P <maheshavp12@gmail.com>
2026-04-15 18:46:08 +0530

    exception handling

---
+++ oops/except1.py
@@ -0,0 +1,22 @@
+#Exception handling
+
+def divide_no():
+    try:
+        num1 = int(input("Enter a number : "))
+        num2 = int(input("Enter a number : "))
+        result = num1/num2
+
+    except ZeroDivisionError:
+        print("No can't divide by zero")
+    
+    except ValueError:
+        print("Only enter the numbers")
+    
+    else:
+        print("Division successful")
+        print(f"result = {result}")
+
+    finally:
+        print("Program execution completed")
+
+divide_no()
